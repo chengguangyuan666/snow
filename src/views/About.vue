fc72fe0f59474c2666a887b3da176972de009517
程光远 <wy1316249555@qq.com>
2021-10-28 09:52:27 +0800

    初始化

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an about page</h1>
+    <div  v-for="(item,index) in unitname" :key="index" >
+      <input  v-model="item.name">
+    </div>
+    <div @click="add">anniu</div>
   </div>
 </template>
+<script>
+
+export default {
+  components: {},
+  name: 'About',
+  data () {
+    return {
+      ss: 'er',
+      unitname: [
+        {
+          name: '211'
+        },
+        {
+          name: '11'
+        }
+      ]
+    }
+  },
+  methods: {
+    async init () {
+
+    },
+    add () {
+      let a = 0
+      for (let i = 0; i < 30000; i++) {
+        a = a + 1
+        this.unitname.push({
+          name: a
+        })
+      }
+    }
+  },
+  async mounted () {
+    // await this.init()
+  },
+  watch: {},
+  computed: {
+  }
+}
+</script>
